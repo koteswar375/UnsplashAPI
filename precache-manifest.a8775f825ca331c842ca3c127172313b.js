1473ca4f0eb560e985dc3307c80223a7b624eea9
koti meesala <kotislr75@gmail.com>
2022-01-10 19:31:41 -0600

    Updates

---
+++ precache-manifest.a8775f825ca331c842ca3c127172313b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f318caa100b91da72efe98aecf9487d",
+    "url": "/UnsplashAPI/index.html"
+  },
+  {
+    "revision": "693c60ca10865c9645a9",
+    "url": "/UnsplashAPI/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "d25a28b4f625d84d471b",
+    "url": "/UnsplashAPI/static/css/main.9b3c3a79.chunk.css"
+  },
+  {
+    "revision": "693c60ca10865c9645a9",
+    "url": "/UnsplashAPI/static/js/2.cb72b30b.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/UnsplashAPI/static/js/2.cb72b30b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d25a28b4f625d84d471b",
+    "url": "/UnsplashAPI/static/js/main.83b594e9.chunk.js"
+  },
+  {
+    "revision": "59ed9c6c334c17f6ab09",
+    "url": "/UnsplashAPI/static/js/runtime-main.e52c646a.js"
+  }
+]);
